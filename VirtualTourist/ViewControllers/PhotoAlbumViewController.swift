168c224d9327a5d1fb6d856ad8e741095f994ab7
iOSDeveloper1000 <aseidel-it@posteo.de>
2021-01-13 13:49:10 +0100

    Set up Main Storyboard with navigation

---
+++ VirtualTourist/ViewControllers/PhotoAlbumViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Arno Seidel on 13.01.21.
+//
+
+import Foundation
+import UIKit
+
+
+class PhotoAlbumViewController: UIViewController {
+    
+    @IBOutlet weak var newCollectionButton: UIButton!
+    
+    
+    
+}
---
+++ VirtualTourist/ViewControllers/TravelLocationsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLocationsViewController.swift
+//  VirtualTourist
+//
+//  Created by Arno Seidel on 13.01.21.
+//
+
+import Foundation
+import UIKit
+
+
+class TravelLocationsViewController: UIViewController {
+    
+    
+    
+    
+}
